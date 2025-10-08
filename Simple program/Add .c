bbbc0c9dac4dc437b62459c4a006957033fa136a
saniya20061977 <shikalgarsaniya08@gmail.com>
2025-10-08 16:38:51 +0530

    Addition of the two number

---
+++ Simple program/Add .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int NO1 ,NO2,SUM;
+    printf("\n Enter a 1st number :");
+    scanf("%d",&NO1);
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&NO2);
+
+    SUM = NO1 + NO2;
+    printf("\n Addition of the given number %d is ",SUM);
+    getch();
+    return 0;
+}
