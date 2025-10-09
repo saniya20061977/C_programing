bcc00f0b8a9bf1d5f0de0353dd2649d870689995
saniya20061977 <shikalgarsaniya08@gmail.com>
2025-10-09 19:29:59 +0530

    Sub

---
+++ Simple program/SUB .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1 , no2 ,sub ;
+    printf("\n Enter a 1st number :");
+    scanf("%d",&no1);
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&no2);
+    sub = no1-no2;
+    printf("\n sun of the given number is %d",sub);
+    getch();
+    return 0;
+}
