f687b0d718206cde7610be6dac945093cbba6b66
saniya20061977 <shikalgarsaniya08@gmail.com>
2025-10-10 23:23:30 +0530

    Multiplication of two number

---
+++ Simple program/Mul .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no1,no2,mul;
+    printf("\n Enter a 1st number :");
+    scanf("%d",&no1);
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&no2);
+    mul = no1 * no2;
+    printf("\n Multipication of the given number  is %d",mul);
+    getch();
+    return 0;
+}
