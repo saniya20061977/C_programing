79a47087079049941e83a63ff27a4ebc996e0ded
saniya20061977 <shikalgarsaniya08@gmail.com>
2025-10-11 20:59:35 +0530

    Div

---
+++ Simple program/Div of the two number .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int NO1,NO2,Div;
+    printf("\n Enter the 1st number :");
+    scanf("%d",&NO1);
+    printf("\n Enter the 2nd number:");
+    scanf("%d",&NO2);
+    Div = NO1 / NO2;
+    printf("\n DIV of the given number is %d",Div);
+    getch();
+    return 0;
+}
