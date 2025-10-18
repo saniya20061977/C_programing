0e56bee319b3217d3e0d5a87cb9ba9a1fecb76c3
saniya20061977 <shikalgarsaniya08@gmail.com>
2025-10-18 08:48:38 +0530

    add

---
+++ addition of two number.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n -----------&&&&&&&&&&------------");
+    int no1,no2,sum;
+    printf("\n Enter 1st number :");
+    scanf("%d",&no1);
+    printf("\n Enter 2nd number :");
+    scanf("%d",&no2);
+    sum=no1+no2;
+    printf("\n addition of %d+%d=%d",no1,no2,sum);
+    printf("\n -----------&&&&&&&&&&&-----------");
+    getch();
+    return 0;
+}
